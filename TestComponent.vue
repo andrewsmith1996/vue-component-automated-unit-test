fcffd23641add080abcf6997eda314d0aec329af
Andrew Smith <andrewsmith96@hotmail.co.uk>
2020-04-27 09:13:03 +0100

    Create TestComponent.vue

---
+++ TestComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot :data="someData" :method="someMethod"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({})
+export default class TestComponent extends Vue {
+  someData: string = 'Some Data';
+
+  someMethod(): void {
+    alert('hello');
+  }
+}
+</script>
