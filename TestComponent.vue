4521ac9fb27f88673d6c6ecf988a9864043238f7
Andrew Smith <andrewsmith96@hotmail.co.uk>
2020-04-27 09:14:47 +0100

    Update TestComponent.vue

--- TestComponent.vue
+++ TestComponent.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component({})
 export default class TestComponent extends Vue {
